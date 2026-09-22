520e4ef4830180519eba8f16f7f4b3edb728c6d6
devlikepro <waha@devlike.pro>
2026-09-22 11:21:13 +0700

    version: 2026.9.2

--- src/version.ts
+++ src/version.ts
@@ -48,7 +48,7 @@ function getPlatform() {
 }
 
 export const VERSION: WAHAEnvironment = {
-  version: '2026.9.1',
+  version: '2026.9.2',
   engine: getEngineName(),
   tier: getWAHAVersion(),
   browser: getBrowser(),
